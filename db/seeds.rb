b98333f1e5beca36f5b69a5b0d271a55b397b507
Kyle Delaney <khdelaney99@gmail.com>
2024-09-04 17:37:50 -0400

    add initial rspec test

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,28 +9,28 @@
 #   end
 
 Poster.create(
-    name: "REGRET",
-    description: "Hard work rarely pays off.",
-    price: 89.00,
-    year: 2018,
-    vintage: true,
-    img_url:  "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d"
+  name: "REGRET",
+  description: "Hard work rarely pays off.",
+  price: 89.00,
+  year: 2018,
+  vintage: true,
+  img_url:  "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d"
 )
 
 Poster.create(
-    name: "UNHAPPY",
-    description: "Hard work rarely pays off.",
-    price: 400.00,
-    year: 1738,
-    vintage: true,
-    img_url:  "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d"
+  name: "UNHAPPY",
+  description: "Hard work rarely pays off.",
+  price: 400.00,
+  year: 1738,
+  vintage: true,
+  img_url:  "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d"
 )
 
 Poster.create(
-    name: "BIG SAD",
-    description: "Why smile when you can frown",
-    price: 120.00,
-    year: 2020,
-    vintage: false,
-    img_url:  "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d"
+  name: "BIG SAD",
+  description: "Why smile when you can frown",
+  price: 120.00,
+  year: 2020,
+  vintage: false,
+  img_url:  "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d"
 )
--- spec/requests/api/v1/posters_request_spec.rb
+++ spec/requests/api/v1/posters_request_spec.rb
@@ -1,1 +1,36 @@
-require 'rails_helper'
+require "rails_helper"
+
+describe "Posters API" do
+  it "sends a list of posters" do
+    Poster.create(
+      name: "REGRET",
+      description: "Hard work rarely pays off.",
+      price: 89.00,
+      year: 2018,
+      vintage: true,
+      img_url:  "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d"
+    )
+
+    Poster.create(
+      name: "UNHAPPY",
+      description: "Hard work rarely pays off.",
+      price: 400.00,
+      year: 1738,
+      vintage: true,
+      img_url:  "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d"
+    )
+
+    Poster.create(
+      name: "BIG SAD",
+      description: "Why smile when you can frown",
+      price: 120.00,
+      year: 2020,
+      vintage: false,
+      img_url:  "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d"
+    )
+
+    get "/api/v1/posters"
+
+    expect(response).to be_successful
+  end
+end

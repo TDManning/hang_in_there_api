2cff60cc8ea8096719a672bf058fbb28c9631c8e
Kyle Delaney <khdelaney99@gmail.com>
2024-09-05 09:10:40 -0400

    Test: add initial test for fetching single record

--- spec/requests/api/v1/posters_request_spec.rb
+++ spec/requests/api/v1/posters_request_spec.rb
@@ -34,7 +34,7 @@ describe "Posters API" do
     expect(response).to be_successful
 
     posters = JSON.parse(response.body, symbolize_names: true)
-    # binding.pry
+
     expect(posters[:data].count).to eq(3)
 
     posters[:data].each do |poster|
@@ -51,5 +51,49 @@ describe "Posters API" do
 
     end
   end
-  # it block for accessing individual song
+  it "sends a specified instance of a poster based on id" do
+    Poster.create(
+      name: "REGRET",
+      description: "Hard work rarely pays off.",
+      price: 89.00,
+      year: 2018,
+      vintage: true,
+      img_url:  "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d"
+    )
+
+    Poster.create(
+      name: "UNHAPPY",
+      description: "Hard work rarely pays off.",
+      price: 400.00,
+      year: 1738,
+      vintage: true,
+      img_url:  "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d"
+    )
+
+    Poster.create(
+      name: "BIG SAD",
+      description: "Why smile when you can frown",
+      price: 120.00,
+      year: 2020,
+      vintage: false,
+      img_url:  "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d"
+    )
+
+    get "/api/v1/posters/1"
+
+    posters = JSON.parse(response.body, symbolize_names: true)
+    
+    expect(posters).to be_a(Hash)
+    expect(posters[:data]).to be_a(Hash)
+    expect(posters[:data]).to be_a(Hash)
+    expect(posters[:data][:id]).to eq(1)
+    expect(posters[:data][:type]).to eq(String)
+    expect(posters[:data][:attributes]).to eq(Hash)
+    expect(posters[:data][:attributes][:name]).to eq("REGRET")
+    expect(posters[:data][:attributes][:description]).to eq("Hard work rarely pays off.")
+    expect(posters[:data][:attributes][:price]).to eq(89.00)
+    expect(posters[:data][:attributes][:year]).to eq(2018)
+    expect(posters[:data][:attributes][:vintage]).to eq(true)
+    expect(posters[:data][:attributes][:img_url]).to eq("https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d")
+  end
 end

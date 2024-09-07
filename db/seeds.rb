692e19f0ca50fbee352fd5162523d32ccd2a0ebe
Kyle Delaney <khdelaney99@gmail.com>
2024-09-07 11:48:46 -0400

    fix:add more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,3 +34,30 @@ Poster.create(
   vintage: false,
   img_url:  "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d"
 )
+
+Poster.create(
+  name: "SHAME",
+  description: "Hard work rarely pays off.",
+  price: 11.00,
+  year: 2000,
+  vintage: true,
+  img_url:  "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d"
+)
+
+Poster.create(
+  name: "TERRIBLE",
+  description: "It's too awful to look at.",
+  price: 15.00,
+  year: 1738,
+  vintage: true,
+  img_url:  "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d"
+)
+
+Poster.create(
+  name: "DISASTER",
+  description: "It's a mess and you haven't even started yet.",
+  price: 28.00,
+  year: 2016,
+  vintage: false,
+  img_url:  "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d"
+)
